855c348ee42b071803ab71631a2b83cfd08e3065
mibe99 <111729215+mibe99@users.noreply.github.com>
2022-09-02 09:09:56 +0300

    Update teht6.py

--- teht6.py
+++ teht6.py
@@ -6,8 +6,8 @@ neliöpisteteet = 0
 for i in range(pisteet**2):
     rand_x = random.uniform(-1, 1)
     rand_y = random.uniform(-1, 1)
-    origin_dist = (rand_y**2 + rand_x**2)
-    if origin_dist <= 1:
+    etaisyys = (rand_y**2 + rand_x**2)
+    if etaisyys <= 1:
         ympyräpisteet += 1
     neliöpisteteet += 1
     pi = 4*(ympyräpisteet/neliöpisteteet)
